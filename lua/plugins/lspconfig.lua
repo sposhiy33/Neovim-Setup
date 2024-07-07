563b5ba43f2091fd36ae059bd388704c4359f2c0
Shrey Poshiya <shreyposh@gmail.com>
2024-07-07 09:37:45 -0700

    added lsp and autocompletion

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,28 +1,32 @@
 return {
 	{
-	"williamboman/mason.nvim",
-	config = function()
-		require("mason").setup()
-	end
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
 	},
 
 	{
-    "williamboman/mason-lspconfig.nvim",
-	config = function()
-		require("mason-lspconfig").setup({
-    		ensure_installed = { "lua_ls", }
-		})
-	end
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup()
+		end
 	},
 
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
 			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			lspconfig.lua_ls.setup({ capabilities = capabilities })
+			lspconfig.jedi_language_server.setup({ capabilties = capabilities })
+			lspconfig.tsserver.setup({ capabilties = capabilities })
+
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-			vim.keymap.set({ 'n', 'v' }, '<space>xa', vim.lsp.buf.code_action, {})
-		end
-	}
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<space>xa", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
+
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,4 +2,4 @@ vim.cmd("set nu")
 vim.cmd("set tabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("syntax on")
-
+vim.cmd("set colorcolumn=80")
